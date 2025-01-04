a6da981c98bf90d63f59fe19183c643f4cab6f10
Anthony Young <anthony.r.young@gmail.com>
2025-01-04 10:57:10 -0700

    Reorder top screen for group then elo

--- critic/src/critic_sqlite/procedures/top_criteria.sql
+++ critic/src/critic_sqlite/procedures/top_criteria.sql
@@ -2,5 +2,5 @@ SELECT c.value, t.name, tc.elo FROM title_criteria tc
 JOIN titles t ON t.id == tc.title_id
 JOIN criteria_group c ON c.id == tc.group_id
 WHERE ?1 IS NULL OR c.value == ?1
-ORDER BY tc.elo DESC, c.value ASC
+ORDER BY c.value ASC, tc.elo DESC 
 LIMIT ?2 OFFSET ?3
